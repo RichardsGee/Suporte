d01b23bc0115f0d105978cec36ea415cd93b6c85
RichardsGee <richardsgee68@gmail.com>
2023-12-20 01:58:04 -0300

    Update

--- script.js
+++ script.js
@@ -30,14 +30,11 @@ function copiarResposta(respostaElement, copiarButton) {
     copiarButton.classList.add('copiado');
 
     // Configure um temporizador para remover a classe e reverter o texto após um período de tempo
-    setTimeout(function() {
+    setTimeout(function () {
         copiarButton.innerHTML = '<i class="far fa-copy"></i>'; // Ícone de copiar
         copiarButton.classList.remove('copiado');
-        copiarButton.style.color = '#333'; // Reverte a cor para a cor padrão
+        copiarButton.style.color = '#ffffff'; // Reverte a cor para a cor padrão
     }, 1000); // Isso removerá a classe 'copiado' e reverterá o texto após 1 segundo (você pode ajustar o tempo conforme necessário)
-
-    // Mude a cor do ícone temporariamente
-    copiarButton.style.color = '#FFCA00'; // Cor verde (você pode ajustar a cor conforme desejado)
 }
 
 // Função para carregar as perguntas e respostas do arquivo JSON
@@ -73,6 +70,7 @@ function displayQuestionsAndAnswers(data) {
 
                 var respostaElement = document.createElement('div');
                 respostaElement.classList.add('resposta');
+                respostaElement.classList.add('animate-background'); // Adicione a classe animate-background
 
                 // Use a tag <div> para envolver o texto e aplicar um estilo específico
                 var textoContainer = document.createElement('div');
@@ -96,8 +94,13 @@ function displayQuestionsAndAnswers(data) {
                 var fecharElement = document.createElement('span');
                 fecharElement.classList.add('fechar');
                 fecharElement.innerHTML = '<i class="fas fa-times"></i>'; // Ícone de fechar
-                fecharElement.onclick = function() {
+                fecharElement.onclick = function () {
                     closeAnswer(this.parentElement);
+                    // Encontre a pergunta correspondente e remova a classe "clicada"
+                    var perguntaClicada = this.parentElement.previousElementSibling;
+                    perguntaClicada.classList.remove('clicada');
+                    perguntaClicada.style.backgroundColor = ''; // Remove o fundo da pergunta fechada
+                    perguntaClicada.style.color = '#ffffff'; // Reverte a cor da fonte da pergunta fechada
                 };
                 respostaElement.appendChild(fecharElement);
 
@@ -105,7 +108,7 @@ function displayQuestionsAndAnswers(data) {
                 var copiarButton = document.createElement('button');
                 copiarButton.classList.add('copiar');
                 copiarButton.innerHTML = '<i class="far fa-copy"></i>'; // Ícone de copiar
-                copiarButton.onclick = function() {
+                copiarButton.onclick = function () {
                     copiarResposta(this.parentElement, this);
                 };
                 respostaElement.appendChild(copiarButton);
@@ -131,9 +134,22 @@ function displayQuestionsAndAnswers(data) {
 
     // Evento de clique para mostrar/ocultar a resposta ao clicar na pergunta
     var perguntas = document.querySelectorAll('.pergunta');
-    perguntas.forEach(function(pergunta) {
-        pergunta.addEventListener('click', function() {
+    perguntas.forEach(function (pergunta) {
+        pergunta.addEventListener('click', function () {
             toggleAnswer(this.nextElementSibling);
+
+            // Fecha todas as outras respostas e remove a classe "clicada" das outras perguntas
+            perguntas.forEach(function (outraPergunta) {
+                if (outraPergunta !== this) {
+                    outraPergunta.classList.remove('clicada');
+                    outraPergunta.style.backgroundColor = ''; // Remove o fundo das outras perguntas
+                    outraPergunta.style.color = '#ffffff'; // Reverte a cor da fonte das outras perguntas
+                }
+            });
+
+            this.classList.toggle('clicada'); // Adiciona ou remove a classe "clicada" na pergunta clicada
+            this.style.backgroundColor = this.classList.contains('clicada') ? '#FFCA00' : ''; // Define o background da pergunta clicada
+            this.style.color = this.classList.contains('clicada') ? '#000000' : '#ffffff'; // Muda a cor da fonte da pergunta clicada
         });
     });
 }
@@ -142,8 +158,9 @@ function displayQuestionsAndAnswers(data) {
 function toggleAnswer(element) {
     var respostas = document.querySelectorAll('.resposta');
 
-    respostas.forEach(function(resposta) {
+    respostas.forEach(function (resposta) {
         resposta.style.display = 'none';
+        resposta.style.opacity = '1'; // Reverte a opacidade para 1 ao alternar a resposta
     });
 
     if (element.style.display === 'block') {
@@ -153,16 +170,30 @@ function toggleAnswer(element) {
     }
 }
 
-// Função para fechar uma resposta específica
+// Função para fechar uma resposta com efeito de transparência
 function closeAnswer(resposta) {
-    resposta.style.display = 'none';
+    var opacity = 1;
+    var interval = setInterval(function () {
+
+        resposta.style.opacity = opacity;
+        opacity -= 0.1; // Reduz a opacidade em 0.1 a cada intervalo
+        if (opacity <= 0) {
+            clearInterval(interval); // Para o intervalo quando a opacidade chegar a 0
+            resposta.style.display = 'none'; // Define a resposta como oculta
+            var perguntas = document.querySelectorAll('.pergunta');
+            perguntas.forEach(function (pergunta) {
+                pergunta.style.backgroundColor = ''; // Remove o fundo da pergunta fechada
+                pergunta.style.color = '#ffffff'; // Reverte a cor da fonte da pergunta fechada
+            });
+        }
+    }, 50); // Intervalo de 50ms para a transição de opacidade (você pode ajustar conforme necessário)
 }
 
 // Seletor para o campo de pesquisa
 var campoPesquisa = document.getElementById('filtro-perguntas');
 
 // Evento de digitação no campo de pesquisa
-campoPesquisa.addEventListener('input', function() {
+campoPesquisa.addEventListener('input', function () {
     var textoPesquisa = campoPesquisa.value.toLowerCase(); // Obtém o texto digitado em letras minúsculas
     filtrarPerguntas(textoPesquisa); // Chama a função de filtro de perguntas
 });
@@ -170,17 +201,39 @@ campoPesquisa.addEventListener('input', function() {
 // Função para filtrar perguntas com base no texto de pesquisa
 function filtrarPerguntas(textoPesquisa) {
     var perguntas = document.querySelectorAll('.pergunta');
-    perguntas.forEach(function(pergunta) {
+    perguntas.forEach(function (pergunta) {
         var perguntaTexto = pergunta.textContent.toLowerCase();
-        var resposta = pergunta.nextElementSibling;
         if (perguntaTexto.includes(textoPesquisa)) {
-            pergunta.style.display = 'block';
-            resposta.style.display = 'none';
+            pergunta.style.display = 'block'; // Mostra a pergunta se corresponder ao filtro
         } else {
-            pergunta.style.display = 'none';
+            pergunta.style.display = 'none'; // Oculta a pergunta se não corresponder ao filtro
         }
     });
 }
+
+// Função para criar e adicionar o botão "Limpar" à caixa de pesquisa
+function adicionarBotaoLimpar() {
+    // Crie o botão "Limpar"
+    var botaoLimpar = document.createElement('button');
+    botaoLimpar.textContent = 'Limpar';
+    
+    // Adicione a classe CSS ao botão "Limpar"
+    botaoLimpar.classList.add('botao-limpar');
+    
+    // Adicione um evento de clique ao botão para limpar o campo de pesquisa
+    botaoLimpar.addEventListener('click', function () {
+        campoPesquisa.value = ''; // Limpa o campo de pesquisa
+        filtrarPerguntas(''); // Chama a função de filtro com uma string vazia para exibir todas as perguntas novamente
+    });
+    
+    // Adicione o botão "Limpar" à caixa de pesquisa
+    var campoPesquisa = document.getElementById('filtro-perguntas');
+    campoPesquisa.parentNode.insertBefore(botaoLimpar, campoPesquisa.nextSibling);
+}
+
+// Chame a função para adicionar o botão "Limpar"
+adicionarBotaoLimpar();
+
 
 // Chamar a função para carregar as perguntas e respostas ao carregar a página
 window.onload = loadQuestionsAndAnswers;
