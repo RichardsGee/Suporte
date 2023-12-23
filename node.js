ad3bd0ea63da26d60b267f0f84715f7788d21a90
RichardsGee <61418034+RichardsGee@users.noreply.github.com>
2023-12-23 13:31:50 -0300

    Update node.js

--- node.js
+++ node.js
@@ -1,36 +1,43 @@
-// URL do endpoint no servidor Node-RED
-const serverEndpointUrl = 'http://194.163.134.51:1880/endpoint/buscar'; // Certifique-se de usar o endereço correto aqui
+// URL do WebSocket no servidor Node-RED
+const serverWebSocketUrl = 'ws://194.163.134.51:1880/endpoint/buscar';
 
 // Credenciais de autenticação
 const username = 'richard'; // Substitua com seu nome de usuário
-const password = 'SuaSenhaAqui'; // Substitua com sua senha
+const password = 'SdkpgVh8LtkbaXm6'; // Substitua com sua senha
 
-// Verifica se a página atual é 'resultado.html' antes de iniciar a solicitação HTTP
+// Verifica se a página atual é 'resultado.html' antes de iniciar o WebSocket
 if (window.location.pathname.endsWith('/resultado.html')) {
     // Obtém o valor da consulta a partir da URL
     const urlParams = new URLSearchParams(window.location.search);
     const consulta = urlParams.get('consulta');
 
-    // Cria um cabeçalho de autenticação para a solicitação HTTP
-    const headers = new Headers({
-        'Authorization': 'Basic ' + btoa(username + ':' + password)
+    // Cria uma nova conexão WebSocket com credenciais
+    const socket = new WebSocket(serverWebSocketUrl, [username, password]);
+
+    // Define as credenciais na conexão WebSocket
+    socket.addEventListener('open', (event) => {
+        console.log('Conexão WebSocket aberta.');
+        
+        // Envia a consulta obtida da URL para o servidor Node-RED
+        socket.send(JSON.stringify({ payload: consulta }));
     });
 
-    // Configura as opções da solicitação HTTP
-    const requestOptions = {
-        method: 'GET', // ou 'POST' se for o caso
-        headers: headers
-    };
+    // Define o que fazer quando uma mensagem é recebida do servidor
+    socket.addEventListener('message', (event) => {
+        console.log('Mensagem recebida do servidor:', event.data);
 
-    // Realiza a solicitação HTTP com autenticação básica
-    fetch(serverEndpointUrl, requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            console.log('Resposta do servidor:', data);
+        // Você pode tratar os dados recebidos aqui e realizar ações com base nas mensagens recebidas
+    });
 
-            // Você pode tratar os dados recebidos aqui e realizar ações com base na resposta
-        })
-        .catch(error => {
-            console.error('Erro na solicitação HTTP:', error);
-        });
+    // Define o que fazer quando ocorre um erro na conexão WebSocket
+    socket.addEventListener('error', (event) => {
+        console.error('Erro na conexão WebSocket:', event);
+    });
+
+    // Define o que fazer quando a conexão WebSocket é fechada
+    socket.addEventListener('close', (event) => {
+        console.log('Conexão WebSocket fechada.');
+
+        // Você pode tentar reconectar aqui, se necessário
+    });
 }
